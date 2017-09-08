431cad03ec71f084c751b49f65e7ea5fc56b00bb
James Noeckel <jwn48@cornell.edu>
2017-09-08 17:45:54 -0400

    added figure

--- tests.jl
+++ tests.jl
@@ -1,1 +1,2 @@
 plot_bounces(x->2, (x,y)->1,x->0,(x,y)->0,(x,y)->0,0,0,normalize([-3, 0.9, 1.01]), 18895, start=18812)
+plot_bounces(x->2, (x,y)->1,x->0,(x,y)->0,(x,y)->0,0,0,normalize([-3, 0.9, 1]), 5000, start=4636)
